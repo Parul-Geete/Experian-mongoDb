3a921c9f5d7ad90a24b365173f1a0a8e2532789e
Parul Geete <parul.geete@gmail.com>
2023-01-05 09:41:45 +0530

    Updated ExperianApplication.java

--- src/main/java/com/experian/experianapi/ExperianApplication.java
+++ src/main/java/com/experian/experianapi/ExperianApplication.java
@@ -1,20 +1,26 @@
 package com.experian.experianapi;
 
-import com.experian.experianapi.repository.CreditCheckRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class })
+@EnableEurekaClient
 @EnableMongoRepositories
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ExperianApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExperianApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExperianApplication.class, args);
-	}
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
